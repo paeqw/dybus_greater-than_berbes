e22bdb7374692f14d3a94ed9786179431658c1ad
paeqw <paeqw12@gmail.com>
2024-02-20 23:12:18 +0100

    berbes to gej

--- app/src/main/java/hate/niggers/MainActivity.java
+++ app/src/main/java/hate/niggers/MainActivity.java
@@ -2,7 +2,9 @@ package hate.niggers;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
@@ -25,8 +27,8 @@ public class MainActivity extends AppCompatActivity {
     ListView listView;
     List<String> items;
     ArrayAdapter<String> itemsAdapter;
-    private static final String PREFS_NAME = "MyPrefs";
-    private static final String KEY_LIST = "list";
+    private static final String PREFS_NAME = "ashbduhasgfdhuga";
+    private static final String KEY_LIST = "kljashdkhjas";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
         editText = findViewById(R.id.editText);
         listView = findViewById(R.id.listView);
         items = new ArrayList<>();
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
         itemsAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, items);
         listView.setAdapter(itemsAdapter);
@@ -44,11 +47,25 @@ public class MainActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                items.remove(i);
-                itemsAdapter.notifyDataSetChanged();
-                saveData();
+                builder.setMessage("Czy chcesz usunac to zadanie?")
+                        .setCancelable(false)
+                        .setPositiveButton("Tak", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                items.remove(i);
+                                itemsAdapter.notifyDataSetChanged();
+                                saveData();
+                            }
+                        })
+                        .setNegativeButton("Nie", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                            }
+                        });
+                AlertDialog alert = builder.create();
+                alert.setTitle("Usuwanie zadania");
+                alert.show();
             }
         });
+
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,6 +73,7 @@ public class MainActivity extends AppCompatActivity {
                 String text = editText.getText().toString();
                 items.add(text);
                 itemsAdapter.notifyDataSetChanged();
+                editText.setText("");
                 saveData();
             }
         });
@@ -79,16 +97,18 @@ public class MainActivity extends AppCompatActivity {
         Set<String> set = new HashSet<>(items);
         editor.putStringSet(KEY_LIST, set);
         editor.apply();
-        Log.wtf("Niggers","Saving...");
     }
 
     private void loadData() {
         SharedPreferences sharedPreferences = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
         Set<String> set = sharedPreferences.getStringSet(KEY_LIST, null);
         if (set != null) {
-            items.addAll(set);
+            Set<String> a = new HashSet<>();
+            a.addAll(items);
+            a.addAll(set);
+            items.clear();
+            items.addAll(a);
             itemsAdapter.notifyDataSetChanged();
         }
-        Log.wtf("Niggers","Loading...");
     }
 }
